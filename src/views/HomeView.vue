7b6dacc7866657f328966b34ebe16c9ff1d4d104
ats527 <truespiritz527@gmail.com>
2025-02-15 12:05:45 +0530

    feat: added Login and Register View with minimal design and functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
 import Button from 'primevue/button'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <template>
-  <main>
-    <HelloWorld msg="Hello World!" />
-    <Button label="Submit" />
+  <main class="w-screen h-screen flex items-center justify-center">
+    <section class="flex flex-col p-4 justify-center items-center">
+      <h1 class="text-3xl">Welcome to our platform</h1>
+      <h2 class="mb-2">Hope you are logged in</h2>
+      <div class="flex gap-4">
+        <Button label="Login" @click="router.push('/login')" />
+        <Button label="Register" @click="router.push('/register')" />
+      </div>
+    </section>
   </main>
 </template>
